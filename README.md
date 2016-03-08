fe35c28403440cc9194c119e95f4d5bd894bbffd
Bjoern Zapadlo <bjoern.zapadlo@gmail.com>
2016-03-08 23:27:07 +0100

    form optimization

--- README.md
+++ README.md
@@ -1,7 +1,6 @@
 # ToDo
-Defaults for form fields
-check format of date timestamps
+Migrate to Redux
+Display form only on click
 Adding absences in the frontend => +
 delete abscences in the frontend => open are you sure
-Migrate to Redux
 Make it run on the raspberry
--- javascript/client/components/AbsenceItemAddComponent.js
+++ javascript/client/components/AbsenceItemAddComponent.js
@@ -1,20 +1,24 @@
 import React from 'react';
 
 class AbsenceItemAddComponent extends React.Component {
+    inputToday() {
+        return (new Date).toISOString().substring(0, 10);
+    }
+
     render() {
         return (
             <div className='box absence-item-add'>
                 <p className='control'>
                     <label className='label'>From</label>
-                    <input className='input' type='date' id='absence-item-add-from' placeholder={Date.now()} />
+                    <input className='input' type='date' id='absence-item-add-from' min={this.inputToday()} defaultValue={this.inputToday()} />
                 </p>
                 <p className='control'>
                     <label className='label'>To</label>
-                    <input className='input' type='date' id='absence-item-add-to' placeholder={Date.now()} />
+                    <input className='input' type='date' id='absence-item-add-to' min={this.inputToday()} defaultValue={this.inputToday()} />
                 </p>
                 <p className='control'>
                     <label className='label'>Location</label>
-                    <input className='input' type='text' id='absence-item-add-location' placeholder='Munich' />
+                    <input className='input' type='text' id='absence-item-add-location' defaultValue='Munich' />
                 </p>
                 <p className='control'>
                     <button className='button is-primary'>Save</button>
